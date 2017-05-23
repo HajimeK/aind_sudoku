82283a9dd6a9320233703fff14f3a3e445e998f0
Hajime <Hajime@10.0.1.16>
2017-05-23 19:51:45 +0900

    Dirty Solution

--- src/function.py
+++ src/function.py
@@ -1,4 +1,6 @@
 from utils import boxes
+from utils import peers
+from utils import unitlist
 
 def grid_values(grid):
     """Convert grid string into {<box>: <value>} dict with '.' value for empties.
@@ -18,3 +20,37 @@ def grid_values(grid):
         elif c in notDeterminedBoxValue:
             elements.append(c)
     return dict(zip(boxes, elements))
+
+def eliminate(values):
+    """Eliminate values from peers of each box with a single value.
+
+    Go through all the boxes, and whenever there is a box with a single value,
+    eliminate this value from the set of values of all its peers.
+
+    Args:
+        values: Sudoku in dictionary form.
+    Returns:
+        Resulting Sudoku in dictionary form after eliminating values.
+    """
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    for box in solved_values:
+        digit = values[box]
+        for peer in peers[box]:
+            values[peer] = values[peer].replace(digit,'')
+    return values
+
+def only_choice(values):
+    """Finalize all values that are the only choice for a unit.
+
+    Go through all the units, and whenever there is a unit with a value
+    that only fits in one box, assign the value to this box.
+
+    Input: Sudoku in dictionary form.
+    Output: Resulting Sudoku in dictionary form after filling in only choices.
+    """
+    for unit in unitlist:
+        for digit in '123456789':
+            dplaces = [box for box in unit if digit in values[box]]
+            if len(dplaces) == 1:
+                values[dplaces[0]] = digit
+    return values
--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 from utils import display
 from function import grid_values
+from function import eliminate
+from function import only_choice
 
-display(grid_values('..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'))
+display(eliminate(eliminate(only_choice(only_choice(eliminate(eliminate(eliminate(eliminate(eliminate(grid_values('..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..')))))))))))
